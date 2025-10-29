7814aa64757fc1c5a807a5487695c7585911290d
Ali <108173040+its4li@users.noreply.github.com>
2025-10-29 11:16:23 +0330

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#6b21a8', // بنفش
+        accent: '#06b6d4',  // آبی-نئونی
+      }
+    },
+  },
+  plugins: [],
+}
